16a200e7c033928eed4be8e490e0f5e92680f41c
Matheus Escudeiro <matheus.escudeiro@outlook.com>
2019-11-07 18:34:07 -0300

    Inserindo o Front-end

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed risus at nunc ornare porttitor.</p>
+    </div>
+)
